bcbd1a1e268ee417e9adf60b5920de01c0f71402
Tarandeep-1270 <77286478+Tarandeep-1270@users.noreply.github.com>
2021-03-23 12:45:27 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    
+//    let ox = document.getElementById("menu");
+//    
+//    if (myFunction(x)) {
+//        ox.style.display = none;
+//    } else {
+//        ox.style.display = block;
+//    }
+}
+
+
+    
+    
